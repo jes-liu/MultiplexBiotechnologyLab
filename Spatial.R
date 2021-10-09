5823ef54b77d0a6ff6fff651a8e989f501de846d
jes-liu <81890734+jes-liu@users.noreply.github.com>
2021-10-08 22:46:04 -0400

    Fixed UMAP clustering

--- Spatial.R
+++ Spatial.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 library(dplyr)
 library(Seurat)
 library(SeuratObject)
@@ -5,13 +7,14 @@ library(patchwork)
 library(openxlsx)
 library(ggplot2)
 
-dir <- "C:/Users/jesse/OneDrive/Documents/Multiplex Lab/Data/Intensities_75CellLogNorm.xlsx"
-#dir <- "C:/Users/jesse/OneDrive/Documents/Multiplex Lab/Data/Intensities_Filtered50.xlsx"
+dir = "C:/Users/jesse/OneDrive/Documents/Multiplex Lab/Data/Intensities_minBG.xlsx"
 
-raw <- data.frame(read.xlsx(dir, sheet=1))
-data_raw <- raw[,2:length(raw)]
+
+raw <- data.frame(read.xlsx(dir))
+data_raw <- raw[,1:length(raw)]
 
 #boxplot(data_raw)
+
 
 data_raw <- t(data.matrix(data_raw))
 
@@ -30,51 +33,64 @@ data_pca <- RunPCA(data_scale, features=VariableFeatures(allData))
 # data_score <- ScoreJackStraw(data_jackstraw, dims=1:15)
 # ElbowPlot(data_score)
 
-data_nn <- FindNeighbors(data_pca, dims=1:9)
-data_clus <- FindClusters(data_nn, resolution=.5)
-data_tsne <- RunTSNE(data_clus, dims=1:9, check_duplicates = FALSE)
+data_nn <- FindNeighbors(data_pca, dims=1:28)
+data_clus <- FindClusters(data_nn)
+data_umap <- RunUMAP(data_clus, dims=1:28)
+# min.dist, spread, n.neighbors
 
-DimPlot(data_tsne, label=TRUE)
-#DimPlot(Embeddings(data_pca)[1:100,1:3], reduction='pca', label=TRUE, group.by='markers')
+DimPlot(data_umap, label=TRUE)
 
-FeaturePlot(data_tsne, features=allProteins, min.cutoff=.75)  # 1400x2200
-
-if(FALSE) {
-  # protein 1 - cluster 0
-  # protein 2 - cluster 17
-  # protein 3 - cluster 28
-  # protein 4 - cluster 16
-  # protein 5 - cluster 20
-  # protein 6 - cluster 19
-  # protein 7 - cluster 11
-  # protein 8 - cluster 0
-  # protein 9 - cluster 24
-  # protein 10 - cluster 6
-  # protein 11 - cluster 22
-  # protein 12 - cluster 18
-  # protein 13 - cluster 29
-  # protein 14 - cluster 14
-  # protein 15 - cluster 27
-  # protein 16 - cluster 23
-  # protein 17 - cluster 3
-  # protein 18 - cluster 2+5
-  # protein 21 - cluster 21
-  # protein 22 - cluster 7 + parts of 1
-  # protein 23 - cluster 13 + parts of 1
-  # protein 24 - cluster 15
-  # protein 25 - cluster 4
-  # protein 26 - cluster 12
-  # protein 27 - cluster 8
-  # protein 28 - cluster 25
-  # protein 29 - cluster 10
-  # protein 30 - cluster 9
-  # protein 31 - cluster 26
-  }
+FeaturePlot(data_umap, features=allProteins)  # 1400x2200
+# min.cutoff=
 
 
+# # get hist of num of values per cell in cluster 0
+# # get hist of the value distribution in cluster 0
+# index = which(data_tsne$seurat_clusters == 0)
+# data_re = data.frame(t(data_raw))
+# subset = data_re[index,]
+# nrow = nrow(subset)
+# ncol = ncol(subset)
+# 
+# array = vector()
+# values = vector()
+# for (x in 1:nrow) {
+#   num = 0
+#   for (y in 1:ncol) {
+#     if (subset[x, y] > 0) {
+#       num = num + 1
+#       values = append(values, subset[x, y])
+#     }
+#   }
+#   array = append(array, num)
+# }
+# hist(array)
+# hist(values)
+# 
+# # get hist of num of values per cell of all else
+# # get hist of the value distribution of all else
+# not_index = which(data_tsne$seurat_clusters != 0)
+# not_subset = data_re[not_index,]
+# not_nrow = nrow(not_subset)
+# 
+# not_array = vector()
+# not_values = vector()
+# for (x in 1:not_nrow) {
+#   num = 0
+#   for (y in 1:ncol) {
+#     if (not_subset[x, y] > 0) {
+#       num = num + 1
+#       not_values = append(not_values, not_subset[x, y])
+#     }
+#   }
+#   not_array = append(not_array, num)
+# }
+# hist(not_array)
+# hist(not_values)
 
-coords <- data.frame("x" = data_tsne[["tsne"]]@cell.embeddings[,1],
-                     "y" = data_tsne[["tsne"]]@cell.embeddings[,2],
-                     "cluster" = data_tsne$seurat_clusters)
 
-write.xlsx(coords, "TSNE_coords.xlsx")
+coords <- data.frame("cell" = 1:length(data_umap$orig.ident),
+                     "seurat_clusters" = data_umap$seurat_clusters)
+
+write.xlsx(coords, "C:/Users/jesse/OneDrive/Documents/Multiplex Lab/Data/UMAP_clusters.xlsx",
+           overwrite=TRUE)
